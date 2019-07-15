4d0a6c46c7001bd948e03563901af1705d409e80
TinaTiel <TinaTielWasTaken@gmail.com>
2019-07-15 20:35:21 +0200

    Added messaging system after all...

--- Command.py
+++ Command.py
@@ -3,20 +3,38 @@ import shlex
 
 class Command():
 
-	def __init__(self, name, description, actions, restrictions):
+	def __init__(self, name, description, aliases=[], actions=[], restrictions=[]):
 		self.name = name
 		self.description = description
+		self.aliases = aliases if isinstance(aliases, list) else []
 		self.actions = actions if isinstance(actions, list) else []
 		self.restrictions = restrictions if isinstance(restrictions, list) else []
 
 	def execute(self, user, args):
+		# If not permitted, fail immediately
+		if(not self._permit(user)):
+			return Result(State.FAILURE)
+
+		# Parse the args
 		results = []
 		args_list = []
 		if(args is not None):
 			args_list = shlex.split(args)
+
+		# Execute each action with user and args
 		for action in self.actions:
-			results.append(action.execute(user, args_list))
-		return results
+			result = action.execute(user, args_list)
+			results.append(result)
+
+			# If an action failed, exit early
+			if(result.state == State.FAILURE):
+				return Result(State.FAILURE, results)
+
+		# Return success
+		return Result(State.SUCCESS, results)
+
+	def _permit(self, user):
+		pass
 
 
 class Action():
@@ -29,9 +47,18 @@ class Action():
 
 
 class Restriction():
-	pass
+	def __init__(self):
+		pass
+
+	def permit(self, user):
+		pass
+
+class Result():
+	def __init__(self, state, messages=[]):
+		self.state = state
+		self.messages = messages
 
 
-class Result(Enum):
+class State(Enum):
 	FAILURE = 1
 	SUCCESS = 2
--- tests/TestCommands.py
+++ tests/TestCommands.py
@@ -8,6 +8,72 @@ class TestCommands(unittest.TestCase):
 	def setUp(self):
 		pass
 
+	def test_restrictions_none(self):
+		'''
+		A Command can have any number of restrictions, which either
+		succeed or fail. If any fails, then the command doesn't execute.
+		'''
+		user = User()
+		action = Action()
+		action.execute = MagicMock()
+
+		# Given a command with no restrictions
+		commandNoRestrictions = Command("name", "descr", ["alias"], [action], None)
+		self.assertEqual(0, len(commandNoRestrictions.restrictions))
+
+		# When executed
+		result = commandNoRestrictions.execute(user, None).state
+
+		# Then the command excecutes available actions & returns SUCCESS
+		self.assertEqual(State.SUCCESS, result)
+		commandNoRestrictions.actions[0].execute.assert_called()
+
+	def test_restrictions_passing(self):
+		'''
+		A Command can have any number of restrictions, which either
+		succeed or fail. If any fails, then the command doesn't execute.
+		'''
+		user = User()
+		action = Action()
+		action.execute = MagicMock()
+
+		# Given a command with passing restrictions
+		restrictionPass = Restriction()
+		restrictionPass.permit = MagicMock(return_value=True)
+		commandPass = Command("name", "descr", ["alias"], [action], [restrictionPass, restrictionPass])
+		self.assertEqual(2, len(commandPass.restrictions))
+
+		# When executed
+		result = commandPass.execute(user, None).state
+
+		# Then the command executes available actions
+		self.assertEqual(State.SUCCESS, result)
+		commandPass.actions[0].execute.assert_called()
+
+	def test_restrictions_failing(self):
+		'''
+		A Command can have any number of restrictions, which either
+		succeed or fail. If any fails, then the command doesn't execute.
+		'''
+		user = User()
+		action = Action()
+		action.execute = MagicMock()
+
+		# Given a command with a failing restriction
+		restrictionPass = Restriction()
+		restrictionPass.permit = MagicMock(return_value=True)
+		restrictionFail = Restriction()
+		restrictionFail.permit = MagicMock(return_value=False)
+		commandFail = Command("name", "descr", ["alias"], [action], [restrictionPass, restrictionFail, restrictionPass])
+		self.assertEqual(3, len(commandFail.restrictions))
+
+		# When executed
+		result = commandFail.execute(user, None).state
+
+		# Then the command does NOT execute any available actions
+		self.assertEqual(State.FAILURE, result)
+		commandFail.actions[0].execute.assert_not_called()
+
 	def test_actions_many_args(self):
 		'''
 		Arguments are separated by spaces
@@ -18,7 +84,7 @@ class TestCommands(unittest.TestCase):
 		restriction = Restriction()
 		action = Action()
 		action.execute = MagicMock()
-		command = Command("test", "descr", [action, action, action], restriction)
+		command = Command("name", "descr", ["alias"], [action, action, action], restriction)
 		self.assertEqual(3, len(command.actions))
 		
 		# When the command is executed with many args
@@ -38,7 +104,7 @@ class TestCommands(unittest.TestCase):
 			restriction = Restriction()
 			action = Action()
 			action.execute = MagicMock()
-			command = Command("test", "descr", [action, action, action], restriction)
+			command = Command("name", "descr", ["alias"], [action, action, action], restriction)
 			self.assertEqual(3, len(command.actions))
 			
 			# When the command is executed with no args
@@ -47,72 +113,6 @@ class TestCommands(unittest.TestCase):
 			# Then each action is executed with no args
 			for action in command.actions:
 				action.execute.assert_called_with(user, [])
-
-	def test_restrictions_none(self):
-		'''
-		A Command can have any number of restrictions, which either
-		succeed or fail. If any fails, then the command doesn't execute.
-		'''
-		user = User()
-		action = Action()
-		action.execute = MagicMock()
-
-		# Given a command with no restrictions
-		commandNoRestrictions = Command("test", "descr", [action], None)
-		self.assertEqual(0, len(commandNoRestrictions.restrictions))
-
-		# When executed
-		result = commandNoRestrictions.execute(user, None)
-
-		# Then the command excecutes available actions & returns SUCCESS
-		self.assertEqual(Result.SUCCESS, result)
-		commandNoRestrictions.actions[0].execute.assert_called()
-
-	def test_restrictions_passing(self):
-		'''
-		A Command can have any number of restrictions, which either
-		succeed or fail. If any fails, then the command doesn't execute.
-		'''
-		user = User()
-		action = Action()
-		action.execute = MagicMock()
-
-		# Given a command with passing restrictions
-		restrictionPass = Restriction()
-		restrictionPass.permit = MagicMock(return_value=True)
-		commandPass = Command("test", "descr", [action], [restrictionPass, restrictionPass])
-		self.assertEqual(2, len(commandPass.restrictions))
-
-		# When executed
-		result = commandPass.execute(user, None)
-
-		# Then the command executes available actions
-		self.assertEqual(Result.SUCCESS, result)
-		commandPass.actions[0].execute.assert_called()
-
-	def test_restrictions_failing(self):
-		'''
-		A Command can have any number of restrictions, which either
-		succeed or fail. If any fails, then the command doesn't execute.
-		'''
-		user = User()
-		action = Action()
-		action.execute = MagicMock()
-
-		# Given a command with a failing restriction
-		restrictionPass = Restriction()
-		restrictionPass.permit = MagicMock(return_value=True)
-		restrictionFail = Restriction()
-		restrictionFail.permit = MagicMock(return_value=False)
-		commandFail = Command("test", "descr", [action], [restrictionPass, restrictionFail, restrictionPass])
-		self.assertEqual(3, len(commandFail.restrictions))
-
-		# When executed
-		result = commandFail.execute(user, None)
-
-		# Then the command does NOT execute any available actions
-		self.assertEqual(Result.FAILURE, result)
-		commandFail.actions[0].execute.assert_not_called()
 
 if __name__ == '__main__':
 	unittest.main()
