dc959891a63e41f21b981c137057f3bf03949089
TinaTiel <TinaTielWasTaken@gmail.com>
2019-07-16 14:21:18 +0200

    Added functionality for action level restrictions such as for progressive voting

--- bot/Action.py
+++ bot/Action.py
@@ -1,3 +1,5 @@
+from bot.Result import *
+
 class Action():
 
 	def __init__(self, restrictions=[], config={}):
@@ -6,12 +8,21 @@ class Action():
 		self.config = config
 
 	def execute(self, user, args):
-		pass
+		if(not self._permit(user)):
+			return Result(State.FAILURE, ["Failed restrictions"])
+		self._execute(user, args)
+		return Result(State.SUCCESS)
 
 	def add_restrictions(self, restrictions):
 		for restriction in restrictions:
 			self.restrictions.append(restriction)
 			restriction.action = self
+
+	def _permit(self, user):
+		for restriction in self.restrictions:
+			if not restriction.permit(user):
+				return False
+		return True
 
 	def _execute(self, user, args):
 		pass
--- bot/Command.py
+++ bot/Command.py
@@ -1,5 +1,5 @@
-from enum import Enum
 import shlex
+from bot.Result import *
 
 class Command():
 
@@ -51,15 +51,3 @@ class Command():
 			if not restriction.permit(user):
 				return False
 		return True
-
-
-class Result():
-	def __init__(self, state, messages=[]):
-		self.state = state
-		self.messages = messages
-
-
-class State(Enum):
-	FAILURE = 1
-	SUCCESS = 2
-	WORKING = 3
