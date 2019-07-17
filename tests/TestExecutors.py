16fa9c8a97b97caba88b5c083279a06afc864253
TinaTiel <TinaTielWasTaken@gmail.com>
2019-07-17 09:43:16 +0200

    Forgot to include the executor tests file in last commit

---
+++ tests/TestExecutors.py
@@ -0,0 +1,43 @@
+import unittest
+from unittest.mock import *
+from bot import *
+
+class TestExecutors(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+	def test_default_executor_success(self):
+		'''
+		Default executor is to execute all Actions in order per request
+		'''
+		# Given the default Executor and a list of Actions
+		a1 = Action()
+		a2 = Action()
+		a3 = Action()
+		a1.execute = MagicMock(return_value=Result(State.SUCCESS))
+		a2.execute = MagicMock(return_value=Result(State.SUCCESS))
+		a3.execute = MagicMock(return_value=Result(State.SUCCESS))
+		executor = Executor([a1, a2, a3])
+
+		# When executed 
+		result = executor.execute(User("foo"), None)
+
+		# Then each action is executed in order
+		a1.execute.assert_called_once()
+		a2.execute.assert_called_once()
+		a3.execute.assert_called_once()
+
+		# And when all execute then a SUCCESS is returned
+		self.assertEqual(State.SUCCESS, result.state)
+		self.assertEqual(3, len(result.messages))
+
+	def test_gated_executor(self):
+		'''
+		Gated executor executes one action per request, only advancing to the 
+		next action at the next request if the prior action executed with SUCCESS
+		'''
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
