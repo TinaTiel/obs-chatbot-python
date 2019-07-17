f8105de10aac760d29696593cf1ff6c04b2071b1
TinaTiel <TinaTielWasTaken@gmail.com>
2019-07-17 09:44:27 +0200

    Forgot to include the executor tests file in last commit

--- bot/Executor.py
+++ bot/Executor.py
@@ -1,9 +1,21 @@
+from bot.Result import *
+
 class Executor():
 	def __init__(self, actions):
-		pass
+		self.actions = actions
 
 	def execute(self, user, args):
-		pass
+		results = []
+		for action in self.actions:
+			result = action.execute(user, args_list)
+			results.append(result)
+
+			# If an action failed, exit early
+			if(result.state == State.FAILURE):
+				return Result(State.FAILURE, results)
+
+		# Return success
+		return Result(State.SUCCESS, results)
 
 class ExecutorGated():
 	def __init__(self, actions):
