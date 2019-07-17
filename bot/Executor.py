4072bf43f62c086b0735db543f0e1f0a97764d66
TinaTiel <TinaTielWasTaken@gmail.com>
2019-07-17 10:41:46 +0200

    Had to update the test because I realized MagicMock doesn't reset call count between each call...which in retrospect is obvious but not to this silly poopyhead.

--- bot/Executor.py
+++ bot/Executor.py
@@ -1,4 +1,5 @@
 from bot.Result import *
+from collections import deque
 
 class Executor():
 	def __init__(self, actions):
@@ -19,7 +20,26 @@ class Executor():
 
 class GatedExecutor():
 	def __init__(self, actions):
-		pass
+		self.executed = list()
+		self.actions = deque(actions)
 
 	def execute(self, user, args_list):
-		pass
+		# If this was initialized with no actions at all, do nothing
+		if(len(self.actions) == 0 and len(self.executed) == 0):
+			return Result(State.SUCCESS)
+
+		# Otherwise if just the actions are empty, then re-initialize them
+		if(len(self.actions) == 0):
+			[self.actions.appendleft(x) for x in self.executed.pop()]
+
+		# process the first item in the queue
+		# if it executes successfully then added it to the executed queue
+		# else put it back in the queue
+		selected = self.actions.popleft()
+		result = selected.execute(user, args_list)
+		if(result.state != State.SUCCESS):
+			self.actions.appendleft(selected)
+		else:
+			self.executed.append(selected)
+
+		return Result(result.state, [result])
