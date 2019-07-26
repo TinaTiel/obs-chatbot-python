d2a58b0e51608af630bf915f40786f11036f1143
TinaTiel <TinaTielWasTaken@gmail.com>
2019-07-26 11:07:46 +0200

    Added tests outlining function of the Command Client

--- tests/TestCommandClient.py
+++ tests/TestCommandClient.py
@@ -7,5 +7,28 @@ class TestCommandClient(unittest.TestCase):
 	def setUp(self):
 		pass
 
-	def test_something(self):
+	def test_command_args(self):
+		'''
+		Every command requires name, allows, and execute. All other stuff is optional.
+		'''
+		self.fail("not implemented")
+
+	def test_command_retrieval(self):
+		'''
+		A command added successfully will be accessible in the client
+		'''
+		self.fail("not implemented")
+
+	def test_command_disable_enable(self):
+		'''
+		Commands can be disabled/enabled during runtime and won't be available to execute
+		'''
+		self.fail("not implemented")
+
+	def test_command_reload(self):
+		'''
+		Commands can be reloaded during runtime, e.g. if the broadcaster changes 
+		something during the broadcast and wants those changes available without
+		restarting the entire bot.
+		'''
 		self.fail("not implemented")
