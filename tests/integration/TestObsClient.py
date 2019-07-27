add28fcd01d79daca3c15fe31eb0ed0ead081596
TinaTiel <TinaTielWasTaken@gmail.com>
2019-07-27 11:08:01 +0200

    Moved unit tests into separate folder, added integration test stub for OBS client

---
+++ tests/integration/TestObsClient.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest.mock import *
+from bot import *
+
+class TestObsClient(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+	def test_something(self):
+		self.fail('foo')
