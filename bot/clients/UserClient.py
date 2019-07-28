65f0dea65ce16f7b9fbe8e5e7c7930310e5a04ef
TinaTiel <TinaTielWasTaken@gmail.com>
2019-07-28 11:44:57 +0200

    Overhauled user client tests to be more SRP

--- bot/clients/UserClient.py
+++ bot/clients/UserClient.py
@@ -1,11 +1,37 @@
 from bot.User import User
+from bot.clients.twitch.TwitchBotCore import TwitchUser
 
 class UserClient():
 	def __init__(self):
 		self.users = {}
 
-	def get_user(self, username):
+	def get_user(self, user):
+		'''
+		User can be a string or TwitchUser
+		If it's a string, just get the user that exists
+		Otherwise update the existing object
+		'''
+		if(isinstance(user, str)):
+			return self.get_user_by_username(user)
+		elif(isinstance(user, TwitchUser)):
+			return self.create_or_update_user(user)
+		else:
+			raise ValueError("User object isn't string or TwitchUser instance, cannot get user!")
+
+	def get_user_by_username(self, string):
+		# return self.users.get(username, None)
 		pass
 
-	def upsert_user(self, twitch_user):
+	def create_or_update_user(self, twitch_user):
+		# existing_user = self.users.get(twitch_user['name'], None)
+		# if(existing_user is None):
+		# 	return self._create_user(twitch_user)
+		# else:
+		# 	return self._update_user(existing_user, twitch_user)
+		pass
+
+	def create_user(self, twitch_user):
+		pass
+
+	def update_user(self, existing_user, twitch_user):
 		pass
--- tests/unit/TestUserClient.py
+++ tests/unit/TestUserClient.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import MagicMock
 from bot.clients.twitch.TwitchBotCore import TwitchUser
 from bot.User import User
 from bot.clients.UserClient import UserClient
@@ -13,23 +14,115 @@ class TestUserClient(TestCase):
 		When creating or updating an user, it re-uses an existing instance if 
 		it exists but if not will create it.
 		'''
-		# Given an public user named tuple from the Twitch bot (implementation may change)
+		# Given a fresh user client
+		client = UserClient()
+		client.get_user_by_username = MagicMock()
+		client.create_or_update_user = MagicMock()
+
+		# When we try to get an user by username
+		user = client.get_user('foo')
+
+		# Then get user by username is called
+		self.assertEqual(1, client.get_user_by_username.call_count)
+		self.assertEqual(0, client.create_or_update_user.call_count)
+
+		# When we try to get an user by twitch_user object
 		twitch_user = TwitchUser("foo",
 													False,
 													False,
 													0,
 													False,
 													False)
-		# And no users by that name in the user client
+		user = client.get_user(twitch_user)
+
+		# Then a it tries to create or update an existing user
+		self.assertEqual(1, client.get_user_by_username.call_count)
+		self.assertEqual(1, client.create_or_update_user.call_count)
+
+		# And if one tries to retrive something that isn't a string or twitch_user
+		# Then a ValueError is thrown
+		self.assertRaises(ValueError, client.get_user, {'name': 'foo'})
+
+	def test_get_user_by_username(self):
+		# Given a client with no users in it
 		client = UserClient()
 		self.assertEqual(0, len(client.users))
 
-		# When this user is upserted
-		client.upsert_user(twitch_user)
+		# When getting an user by username
+		user = client.get_user_by_username('foo')
 
-		# Then a new user is created with additional metadata
+		# Then None is returned
+		self.assertEqual(None, user)
+
+		# But when that user exists
+		client.users['foo'] = User('foo')
 		self.assertEqual(1, len(client.users))
-		user = client.get_user(twitch_user['name'])
+
+		# When getting that user by username
+		user = client.get_user('foo')
+
+		# Then the user is returned
+		self.assertTrue(isinstance(user, User))
+		self.assertTrue('foo', user.username)
+		self.assertEqual(False, user.follower)
+		self.assertEqual(False, user.subscriber)
+		self.assertEqual(False, user.subscriber_dur)
+		self.assertEqual(False, user.moderator)
+		self.assertEqual(False, user.broadcaster)
+		self.assertEqual(0, user.points)
+
+	def test_create_or_update(self):
+
+		# Given a clean user client
+		client = UserClient()
+		client.create_user = MagicMock()
+		client.update_user = MagicMock()
+		self.assertEqual(0, len(client.users))
+
+		# And a twitch user
+		twitch_user = TwitchUser("foo",
+													False,
+													False,
+													0,
+													False,
+													False)
+
+		# When we try to create or update a twitch user
+		client.create_or_update_user(twitch_user)
+
+		# Then create_user is called
+		self.assertEqual(1, client.create_user.call_count)
+		self.assertEqual(0, client.update_user.call_count)
+
+		# But if an user by that username exists
+		client.users['foo'] = User('foo')
+		self.assertEqual(1, len(client.users))
+
+		# When we try to create or update a twitch user
+		client.create_or_update_user(twitch_user)
+
+		# Then update_user is called
+		self.assertEqual(1, client.create_user.call_count)
+		self.assertEqual(1, client.update_user.call_count)
+
+	def test_create_user(self):
+		# Given a clean client
+		client = UserClient()
+		self.assertEqual(0, len(client.users))
+
+		# And a Twitch user
+		twitch_user = TwitchUser("foo",
+													False,
+													False,
+													0,
+													False,
+													False)
+
+		# When we create an user
+		user = client.create_user(twitch_user)
+
+		# Then the user is created
+		self.assertTrue(isinstance(user, User))
 		self.assertEqual('foo', user.username)
 		self.assertEqual(False, user.follower)
 		self.assertEqual(False, user.subscriber)
@@ -38,21 +131,30 @@ class TestUserClient(TestCase):
 		self.assertEqual(False, user.broadcaster)
 		self.assertEqual(0, user.points)
 
-		# When that same user but with new info is upserted again (is a follower)
+	def test_update_user(self):
+		# Given a client with an existing user
+		client = UserClient()
+		existing_user = User('foo')
+		client.users['foo'] = existing_user
+		self.assertEqual(1, len(client.users))
+
+		# And an updated Twitch user (now following)
 		updated_twitch_user = TwitchUser("foo",
 													True,
 													False,
 													0,
 													False,
 													False)
-		same_user = client.upsert(updated_twitch_user)
 
-		# Then the existing user is updated
-		self.assertTrue(same_user is user) # same memory addr
-		self.assertEqual('foo', same_user.username)
-		self.assertEqual(True, same_user.follower)
-		self.assertEqual(False, same_user.subscriber)
-		self.assertEqual(False, same_user.subscriber_dur)
-		self.assertEqual(False, same_user.moderator)
-		self.assertEqual(False, same_user.broadcaster)
-		self.assertEqual(0, same_user.points)
+		# When we update a Twitch user
+		updated_user = client.update_user(existing_user, updated_twitch_user)
+
+		# Then the existing user object is updated
+		self.assertTrue(updated_user is existing_user) # same object in memory
+		self.assertEqual('foo', updated_user.username)
+		self.assertEqual(True, updated_user.follower)
+		self.assertEqual(False, updated_user.subscriber)
+		self.assertEqual(False, updated_user.subscriber_dur)
+		self.assertEqual(False, updated_user.moderator)
+		self.assertEqual(False, updated_user.broadcaster)
+		self.assertEqual(0, updated_user.points)
